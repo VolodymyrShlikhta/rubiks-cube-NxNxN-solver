7068dd586f5b08bf8ee011a66da900a33fad9145
vshlikhta <vol.shlikhta@gmail.com>
2026-04-06 16:28:47 +0300

    windows: replace wget/gunzip with urllib.request and gzip
    wget and gunzip are not available on stock Windows. Use urllib.request.urlretrieve for download and the stdlib gzip module for decompression.

--- rubikscubennnsolver/LookupTable.py
+++ rubikscubennnsolver/LookupTable.py
@@ -3,10 +3,12 @@ import datetime as dt
 import hashlib
 import json
 import logging
+import gzip
 import os
 # import resource  # removed for Windows compatibility
 import shutil
 import subprocess
+import urllib.request
 from pathlib import Path
 from subprocess import call
 from typing import Dict, List, TextIO, Tuple
@@ -428,16 +430,16 @@ def download_file_if_needed(filename: str) -> None:
 
         if not os.path.exists(filename_gz):
             url = f"https://rubiks-cube-lookup-tables.s3.amazonaws.com/{filename_gz_no_dir}"
-            logger.info(f"Downloading table via 'wget {url}'")
-            call(["wget", url])
+            logger.info(f"Downloading table via urllib: {url}")
+            urllib.request.urlretrieve(url, filename_gz)
 
-            if not os.path.exists(filename_gz_no_dir):
+            if not os.path.exists(filename_gz):
                 raise Exception(f"failed to download {filename_gz} via {url}")
 
-            shutil.move(filename_gz_no_dir, filename_gz)
-
         logger.info(f"gunzip {filename_gz}")
-        call(["gunzip", filename_gz])
+        with gzip.open(filename_gz, "rb") as f_in, open(filename, "wb") as f_out:
+            shutil.copyfileobj(f_in, f_out)
+        os.remove(filename_gz)
 
 
 class LookupTable(object):
